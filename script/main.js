9e57e716aa32feb3f7b68802991f544e75a7c331
DavidCardonaG <softwarewd@outlook.com>
2021-08-18 13:38:15 -0500

    load lets

--- script/main.js
+++ script/main.js
@@ -3,8 +3,15 @@ import Planet from "../script/planeta.js";
 
 
 var arrayValores;
-let arrayDistances;
+let arrayDistance;
 let arrayNamePlanets = [];
 let arrayPlanets = [];
 let arrayTamañ = [];
 
+var enviarEvaluacion = (e) => {
+
+    arrayValores = [...document.querySelectorAll(".numeracion_planetas").value];
+    arrayDistance = [...document.querySelectorAll(".distancia_planetas").value];
+    arrayNamePlanets = [...document.querySelectorAll(".nombre_planetas").value];
+    arrayTamañ = [...document.querySelectorAll(".Tamanio_planetas").value];
+}
